640c90a205aea3a0620699f9ce21c0768302bd12
yudong <yoshino1347716570@gmail.com>
2019-10-28 06:55:13 +0800

    Create MaxMinAndAverage.js

---
+++ PreMEAN/MaxMinAndAverage.js
@@ -0,0 +1,17 @@
+function maxMinAvg(arr){
+    var max = arr[0];
+    var min = arr[0];
+    var sum = 0;
+    for(i=0;i<arr.length;i++){
+        if(min>arr[i]){
+            min = arr[i];
+        }
+        if(max<arr[i]){
+            max = arr[i];
+        }
+        sum+=arr[i]
+    }
+    var avg = sum/arr.length
+    return("The minimum is "+min+", the maximum is "+max+", and the average is "+avg+".")
+}
+console.log(maxMinAvg([1, -2, 9, 4]))
