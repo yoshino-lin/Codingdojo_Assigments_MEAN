e587037056dbce029b6444de619992c05f243924
yudong <yoshino1347716570@gmail.com>
2019-10-29 06:25:05 +0800

    Update HundredAcreWood.js

--- PreMEAN/HundredAcreWood.js
+++ PreMEAN/HundredAcreWood.js
@@ -1,14 +1,47 @@
-var tigger = { character: "Tigger" };
-var pooh = { character: "Winnie the Pooh" };
-var piglet = { character: "Piglet" };
-var bees = { character: "Bees" };
-var owl = { character: "Owl" };
-var robin = { character: "Christopher Robin" };
-var rabbit = { character: "Rabbit" };
-var gopher = { character: "Gopher" };
-var kanga = { character: "Kanga" };
-var eeyore = { character: "Eeyore" };
-var heffalumps = { character: "Heffalumps" };
+var tigger={
+    character: "Tigger",
+    greet: function(){console.log("The wonderful thing about Tiggers is Tiggers are wonderful things!")}
+};
+var pooh={
+    character: "Winnie the Pooh",
+    greet: function(){console.log("Hi friend, try some fresh honey!")}
+};
+var piglet={
+    character: "Piglet",
+    greet: function(){console.log("Oh d-d-d-d-dear! I wasn't expecting company!")}
+};
+var bees={
+    character: "Bees",
+    greet: function(){console.log("Ennnnnn! Do you see my honey?")}
+};
+var owl={
+    character: "Owl",
+    greet: function(){console.log("I am so tired... I need more sleep.")}
+};
+var robin={
+    character: "Christopher Robin",
+    greet: function(){console.log("Welcome to my house! Do you want to have dinner with me?")}
+};
+var rabbit={
+    character: "Rabbit",
+    greet: function(){console.log("Guess what do I find today? A egg!")}
+};
+var gopher={
+    character: "Gopher",
+    greet: function(){console.log("Well, you know everyone cannot live without water!")}
+};
+var kanga={
+    character: "Kanga",
+    greet: function(){console.log("Check my baby! Is him cute?")}
+};
+var eeyore={
+    character: "Eeyore",
+    greet: function(){console.log("Hello...And...what is your name again?")}
+};
+var heffalumps={
+    character: "Heffalumps",
+    greet: function(){console.log("I do not know why you are here. But, nice to see you!")}
+};
 tigger.north = pooh;
 pooh.south = tigger;
 pooh.north = robin;
@@ -42,6 +75,7 @@ function move(direction){
         if(player["location"].north!=null){
             player["location"]=player["location"].north
             console.log("Now you are at "+player["location"]["character"]+"'s house")
+            player["location"].greet()
         }else{
             console.log("You may not go that way")
         }
@@ -49,6 +83,7 @@ function move(direction){
         if(player["location"].south!=null){
             player["location"]=player["location"].south
             console.log("Now you are at "+player["location"]["character"]+"'s house")
+            player["location"].greet()
         }else{
             console.log("You may not go that way")
         }
@@ -56,6 +91,7 @@ function move(direction){
         if(player["location"].east!=null){
             player["location"]=player["location"].east
             console.log("Now you are at "+player["location"]["character"]+"'s house")
+            player["location"].greet()
         }else{
             console.log("You may not go that way")
         }
@@ -63,6 +99,7 @@ function move(direction){
         if(player["location"].west!=null){
             player["location"]=player["location"].west
             console.log("Now you are at "+player["location"]["character"]+"'s house")
+            player["location"].greet()
         }else{
             console.log("You may not go that way")
         }
