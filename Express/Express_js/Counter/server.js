7e9a20df66be445d50ea8abd5859d7cb140dcd9a
yudong <yoshino1347716570@gmail.com>
2019-11-01 08:02:06 +0800

    session done

--- Express/Express_js/Counter/server.js
+++ Express/Express_js/Counter/server.js
@@ -1,30 +1,34 @@
 const express = require("express");
 const app = express();
-
+const session = require('express-session');
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
 app.use(express.static(__dirname + "/static"));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-var times = 0;
-
 app.get("/", (req, res) => {
-    if(times == 0){
-        times = 1;
+    if(req.session.times == null){
+        req.session.times = 1;
     }else{
-        times += 1;
+        req.session.times += 1;
     }
     context={
-        how_many_times:times
+        how_many_times:req.session.times
     }
     res.render('index',context);
 })
 
 app.post("/plus_two", (req, res) => {
-    times+=1
+    req.session.times+=1
     res.redirect('/');
 })
 app.post("/reset", (req, res) => {
-    times=0
+    req.session.times=null
     res.redirect('/');
 })
 var port = 8000;
